4b0f44ec7fdd24b57009aa520c1e6d564eb0da2c
Vinny <vincedoanschool@gmail.com>
2026-05-12 12:06:12 -0700

    scope cors to production origin in all edge functions

--- supabase/functions/book-appointment/index.ts
+++ supabase/functions/book-appointment/index.ts
@@ -4,7 +4,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': Deno.env.get('APP_URL') ?? 'https://lbmartialarts.com',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
 
